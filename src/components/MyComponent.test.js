0ec88bd6c9ce5c271b765bef4c0d1d65ac2846c9
Alice Kim <alice.kim@qualia.com>
2025-08-11 22:23:43 -0700

    update

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import MyComponent from './MyComponent';
 
-describe("MyComponent", () => {
-  it("should render correctly", () => {
-    const component = shallow(<MyComponent />);
+describe('MyComponent', () => {
+  it('renders', () => {
+    const wrapper = shallow(<MyComponent />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('matches snapshot', () => {
+    const wrapper = shallow(<MyComponent />);
+    expect(wrapper.debug()).toMatchSnapshot();
+  });
+
+  it('creates state entry on change', () => {
+    const wrapper = shallow(<MyComponent />);
+    const input = wrapper.find('input');
+    input.props().onChange({ target: { name: 'input', value: 'myValue' } });
+    expect(wrapper.state('input')).toBeDefined();
+  });
+
+  it('stores the event value into state', () => {
+    const wrapper = shallow(<MyComponent />);
+    const input = wrapper.find('input');
+    input.props().onChange({ target: { name: 'input', value: 'myValue' } });
+    expect(wrapper.state('input')).toEqual('myValue');
   });
 });
